caba29701165b9497059208be9f02567e6a09155
Rashvandh <arashvandh85@gmail.com>
2025-12-05 08:50:46 +0530

    Fix build configuration

--- netlify/api.js
+++ netlify/api.js
@@ -1,7 +1,6 @@
 const { handler } = require('@netlify/functions');
 const express = require('express');
 const serverless = require('serverless-http');
-
 const app = express();
 app.use(express.json());
 
